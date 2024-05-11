ded93898642ec466a70594ea7659a0b4daad8d08
cshorg <colegrohs@outlook.com>
2024-05-10 20:36:05 -0700

    updated drizzle kit for studio

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'drizzle-kit'
+
 export default defineConfig({
- schema: "./src/db/schema.ts",
+  schema: "./src/db/schema.ts",
+  dialect: 'postgresql',
   driver: 'pg',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
   verbose: true,
   strict: true,
