162e0fae545d8b47a7401f8140ecea1c368f494f
cshorg <colegrohs@outlook.com>
2024-05-17 21:14:07 -0700

    form reqs

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -19,8 +19,8 @@ import { useRouter } from "next/navigation"
 import { useToast } from "@/components/ui/use-toast"
 
 const formSchema = z.object({
-  title: z.string().min(5).max(50),
-  description: z.string().min(2).max(250)
+  title: z.string().min(4).max(25),
+  description: z.string().min(5).max(500)
 })
 
 export function BoardForm({ board }: any) {
